28a54f14555d348f6a5a320f85a9b1fb4cc7da1e
Ivan Martinez <jesulim@gmail.com>
2022-10-03 09:35:51 -0400

    change search

--- services/search.js
+++ services/search.js
@@ -3,11 +3,17 @@ import algoliasearch from 'algoliasearch/lite'
 const client = algoliasearch('JGS1Z5Z2KB', '52a6212717375396f5a38c24be55549e');
 const index = client.initIndex('prod_comics');
 
+const CACHE = {}
+
 export const search = async ({ query }) => {
+  if(CACHE[query]) return { results: CACHE[query]}
+
   const { hits } = await index.search(query, {
     attributesToRetrieve: ['id', 'title', 'img', 'alt'],
     hitsPerPage: 10
   })
+
+  CACHE[query] = hits
 
   return { results: hits }
 }
